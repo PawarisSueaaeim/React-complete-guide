8c60d5b65928eabfa22ea8a2c513627ec5949369
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2022-12-15 22:19:42 +0700

    passing data props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@
 import ExpenseItem from "./components/Expenseltem";
 
 function App() {
+
+  const expense = [
+    {
+      id: "0",
+      title: "Car",
+      price: "10",
+      date: new Date(2022, 2, 1),
+    },{
+      id: "1",
+      title: "pen",
+      price: "1",
+      date: new Date(2022, 2, 2),
+    },{
+      id: "2",
+      title: "water",
+      price: "10",
+      date: new Date(2022, 2, 3),
+    },{
+      id: "3",
+      title: "phone",
+      price: "100",
+      date: new Date(2022, 2, 4),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>
         This is home page
       </h2>
-      <ExpenseItem />
+      <ExpenseItem title={expense[0].title} price={expense[0].price} date={expense[0].date}/>
+      <ExpenseItem title={expense[1].title} price={expense[1].price} date={expense[1].date}/>
+      <ExpenseItem title={expense[2].title} price={expense[2].price} date={expense[2].date}/>
+      <ExpenseItem title={expense[3].title} price={expense[3].price} date={expense[3].date}/>
     </div>
   );
 }
--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,19 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem () {
-
-    const expenseDate = new Date(2022, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expensePrice = '10';
+function ExpenseItem (props) {
 
     return (
         <div className='expense-item'>
-            {expenseDate.toString()}
+            {props.date.toString()}
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
             </div>
             <div className='expense-item__price'>
-                ${expensePrice}
+                ${props.price}
             </div>
         </div>
     );
