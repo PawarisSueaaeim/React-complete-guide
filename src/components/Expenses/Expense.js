d5d4da309828c9393338b64440dedf21b12f6bca
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-01-01 23:20:58 +0700

    filter year

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import './Expense.css';
-import ExpenseItem from './Expenseltem';
-import Card from '../UI/Card';
+import React, {useState} from "react";
+import "./Expense.css";
+import ExpenseItem from "./Expenseltem";
+import Card from "../UI/Card";
+import ExpensesFilter from "../Filter/ExpensesFilter";
 
 function Expense(props) {
 
-    return (
-        <Card className="container">
-            <ExpenseItem title={props.expense[0].title} price={props.expense[0].price} date={props.expense[0].date}/>
-            <ExpenseItem title={props.expense[1].title} price={props.expense[1].price} date={props.expense[1].date}/>
-            <ExpenseItem title={props.expense[2].title} price={props.expense[2].price} date={props.expense[2].date}/>
-            <ExpenseItem title={props.expense[3].title} price={props.expense[3].price} date={props.expense[3].date}/>
-        </Card>
-    );
-};
+    const [thisYear, setThisYear] = React.useState('2020');
 
-export default Expense
+    const filterHandlerChange = (selectedYear) => {
+        setThisYear(selectedYear);
+        console.log(selectedYear);
+    };
+
+  return (
+    <div>
+      <Card className="container">
+      <ExpensesFilter onChangeFilter={filterHandlerChange} yearValue={thisYear}/>
+        <ExpenseItem
+          title={props.expense[0].title}
+          price={props.expense[0].price}
+          date={props.expense[0].date}
+        />
+        <ExpenseItem
+          title={props.expense[1].title}
+          price={props.expense[1].price}
+          date={props.expense[1].date}
+        />
+        <ExpenseItem
+          title={props.expense[2].title}
+          price={props.expense[2].price}
+          date={props.expense[2].date}
+        />
+        <ExpenseItem
+          title={props.expense[3].title}
+          price={props.expense[3].price}
+          date={props.expense[3].date}
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default Expense;
