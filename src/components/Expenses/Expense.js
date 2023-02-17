05f7016758834ba5240c3d22779ed5a3f83747cf
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-02-17 12:00:58 +0700

    refactor code

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -16,26 +16,13 @@ function Expense(props) {
     <div>
       <Card className="container">
       <ExpensesFilter onChangeFilter={filterHandlerChange} yearValue={thisYear}/>
-        <ExpenseItem
-          title={props.expense[0].title}
-          price={props.expense[0].price}
-          date={props.expense[0].date}
-        />
-        <ExpenseItem
-          title={props.expense[1].title}
-          price={props.expense[1].price}
-          date={props.expense[1].date}
-        />
-        <ExpenseItem
-          title={props.expense[2].title}
-          price={props.expense[2].price}
-          date={props.expense[2].date}
-        />
-        <ExpenseItem
-          title={props.expense[3].title}
-          price={props.expense[3].price}
-          date={props.expense[3].date}
-        />
+      {props.expense?.map((expense) => {
+        return (<ExpenseItem
+          title={expense.title}
+          price={expense.price}
+          date={expense.date}
+          />)
+      })}
       </Card>
     </div>
   );
